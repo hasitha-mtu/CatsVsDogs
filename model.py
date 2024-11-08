9dcb251ab6932571ba3c52ff43ce58d5a5ee67af
Hasitha Adikari <adikari.adikari@mycit.ie>
2024-11-08 00:07:33 +0000

    check model accuracy

--- model.py
+++ model.py
@@ -3,8 +3,14 @@ from tensorflow.keras import layers
 
 
 def get_model():
+    data_augmentation = keras.Sequential([
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2)
+    ])
     inputs = layers.Input(shape=(180, 180, 3))
-    x = layers.Rescaling(1./255)(inputs)
+    x = data_augmentation(inputs)
+    x = layers.Rescaling(1./255)(x)
     x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
@@ -15,6 +21,7 @@ def get_model():
     x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
     x = layers.Flatten()(x)
+    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(1, activation="sigmoid")(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     print(f"Model information : {model.summary()}")
@@ -23,5 +30,15 @@ def get_model():
                   metrics=["accuracy"])
     return model
 
+def pre_trained_model():
+    conv_base = keras.applications.vgg16.VGG16(
+        weights="imagenet",
+        include_top=False,
+        input_shape=(180, 180, 3)
+    )
+    conv_base.summary()
+    return conv_base
+
 if __name__ == "__main__":
-    get_model()
+    # get_model()
+    pre_trained_model()
--- train.py
+++ train.py
@@ -1,8 +1,11 @@
+from xml.sax.handler import all_features
+
 import keras.callbacks
 import matplotlib.pyplot as plt
 from preprocessing import load_images
-from model import get_model
+from model import get_model, pre_trained_model
 from datetime import datetime
+import numpy as np
 
 
 def train_model():
@@ -14,7 +17,7 @@ def train_model():
     )
     callbacks = [
         keras.callbacks.ModelCheckpoint(
-            filepath="convnet_from_scratch.keras",
+            filepath="convnet_from_scratch_with_augmentation.keras",
             save_best_only=True,
             monitor="val_loss"
         ),
@@ -22,26 +25,48 @@ def train_model():
     ]
     history = model.fit(
         train_dataset,
-        epochs=30,
+        epochs=100,
         validation_data=validation_dataset,
         callbacks=callbacks
     )
     return history
 
+def get_features_and_labels(dataset):
+    all_features = []
+    all_labels = []
+    conv_base = pre_trained_model()
+    for images, labels in dataset:
+        preprocessed_images = keras.applications.vgg16.preproccess_input(images)
+        features = conv_base.predict(preprocessed_images)
+        all_features.append(features)
+        all_labels.append(labels)
+    return np.concatenate(all_features), np.concatenate(all_labels)
+
+
+def check_test_accuracy():
+    (_, _, test_dataset) = load_images()
+    test_model = keras.models.load_model("convnet_from_scratch_with_augmentation.keras")
+    test_loss, test_acc = test_model.evaluate(test_dataset)
+    print(f"Test accuracy: {test_acc:.3f}")
+    return None
+
+# if __name__ == "__main__":
+#     history = train_model()
+#     accuracy = history.history["accuracy"]
+#     val_accuracy = history.history["val_accuracy"]
+#     loss = history.history["loss"]
+#     val_loss = history.history["val_loss"]
+#     epochs = range(1, len(accuracy) + 1)
+#     plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+#     plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+#     plt.title("Training and validation accuracy")
+#     plt.legend()
+#     plt.figure()
+#     plt.plot(epochs, loss, "bo", label="Training loss")
+#     plt.plot(epochs, val_loss, "b", label="Validation loss")
+#     plt.title("Training and validation loss")
+#     plt.legend()
+#     plt.show()
+
 if __name__ == "__main__":
-    history = train_model()
-    accuracy = history.history["accuracy"]
-    val_accuracy = history.history["val_accuracy"]
-    loss = history.history["loss"]
-    val_loss = history.history["val_loss"]
-    epochs = range(1, len(accuracy) + 1)
-    plt.plot(epochs, accuracy, "bo", label="Training accuracy")
-    plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
-    plt.title("Training and validation accuracy")
-    plt.legend()
-    plt.figure()
-    plt.plot(epochs, loss, "bo", label="Training loss")
-    plt.plot(epochs, val_loss, "b", label="Validation loss")
-    plt.title("Training and validation loss")
-    plt.legend()
-    plt.show()
+    check_test_accuracy()
